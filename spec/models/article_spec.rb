7f5818609bed31324a2e601d5bd2f62168d027fa
Lucas William <lucaswilliam133@gmail.com>
2023-05-17 12:45:16 -0300

    Create tests for articles

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  it 'is valid with valid attributes' do
+    article = Article.new(title: 'Anything',
+                          body: 'Lorem ipsum dolor sit amet.',
+                          reading_time: 5)
+    expect(article).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    article = Article.new(title: nil,
+                          body: 'Lorem ipsum dolor sit amet.',
+                          reading_time: 5)
+    expect(article).to_not be_valid
+  end
+
+  it 'is not valid without a body' do
+    article = Article.new(title: 'Anything',
+                          body: nil,
+                          reading_time: 5)
+    expect(article).to_not be_valid
+  end
+
+  it 'is not valid without a reading_time' do
+    article = Article.new(title: 'Anything',
+                          body: 'Lorem ipsum dolor sit amet.',
+                          reading_time: nil)
+    expect(article).to_not be_valid
+  end
+end
